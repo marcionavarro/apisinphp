3da6a9b856454d1c47f511653042c954634e9942
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-10-10 01:18:30 -0300

    Seção 9: Uma introdução à autenticação usando tokens de acesso parte 2

--- www/src/UserGateway.php
+++ www/src/UserGateway.php
@@ -16,4 +16,15 @@ class UserGateway
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function getByUsername(string $username): array|false
+    {
+        $sql = "SELECT * FROM user WHERE username = :username";
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":username", $username, PDO::PARAM_STR);
+
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
